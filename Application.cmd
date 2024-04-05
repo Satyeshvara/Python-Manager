53bb38b265da9675b15aeb66e65dad2063174272
satishkumarsingh2024 <id.satishkumarsingh@gmail.com>
2024-04-05 23:36:40 +0530

    Update

---
+++ Application.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+set SourceFile=".\Source Files\Python-Manager_v1.3.py"
+python %SourceFile%
--- Python-Manager_v1.3/Application.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-set SourceFile=".\Data\Python-Manager_v1.3.py"
-python %SourceFile%
